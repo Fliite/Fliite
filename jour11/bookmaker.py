dcce45bb76458bae6dfb0f691314240b6967395a
BERRY Lucas <berrylucas333@gmail.com>
2023-03-08 13:24:49 +0000

    done

--- jour11/bookmaker.py
+++ jour11/bookmaker.py
@@ -4,22 +4,16 @@ Code3L = ["ALA" , "CYS" , "ASP" , "GLU" , "PHE" , "GLY" , "HIS" , "ILE" , "LYS" 
 test = ["MET" , "ASN" , "PRO" ,"GLN" , "ARG" , "SER" , "THR" , "VAL" , "TRP" , "TYR"]
 
 
-def codageDesProts(Code1L, Code3L):
+def codageDesProts():
     dico = {}
     for i in range(len(Code1L)):
         dico[Code1L[i]] = Code3L[i]
     return dico
 
-def reverseCodageDesProts(Code1L, Code3L):
+def reverseCodageDesProts():
     dico = {}
     for i in range(len(Code1L)):
         dico[Code3L[i]] = Code1L[i]
     return dico
 
-def retourne1L(test, dico):
-    prot1L = ""
-    for i in test:
-        prot1L += dico[i]
-    return prot1L
-
-retourne1L(test, reverseCodageDesProts(Code1L, Code3L))
+print("codage des ports :",codageDesProts(),"\n\n reverse codage des ports :",reverseCodageDesProts())
