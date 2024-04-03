d3419ccb1dbfc29703d67aaf0a83d9c2c5aeef38
BERRY Lucas <l.berry@eleve.leschartreux.net>
2024-04-03 14:19:13 +0000

    done

--- PROJET INFO/Maversion.py
+++ PROJET INFO/Maversion.py
@@ -2,7 +2,7 @@ import random
 
 # Définition des couleurs et des valeurs des cartes
 couleurs = ['Rouge', 'Vert', 'Bleu', 'Jaune']
-ValCarte = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'SensInterdit', 'Contresen', 'PlusDeux']
+ValCarte = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'SensInterdit', 'Contresens', 'PlusDeux']
 
 # Définition de la classe Carte
 class Card:
@@ -32,9 +32,6 @@ class Player:
     def play_card(self, card_index):
         return self.hand.pop(card_index)
 
-# Initialisation des joueurs
-NbreJoueurs = int(input("Entrez le nombre de joueurs : "))
-Joueurs = [Player(input(f"Entrez le nom du joueur {i+1}: ")) for i in range(NbreJoueurs)]
 
 # Distribution des cartes aux joueurs
 for player in Joueurs:
@@ -80,3 +77,33 @@ while True:
         Joueurs[JoueurActuel].draw_card(deck)
 
     JoueurActuel = (JoueurActuel + direction) % len(Joueurs)
+
+def initialisation():
+    # Initialisation des joueurs
+    NbreJoueurs = int(input("Entrez le nombre de joueurs : "))
+    Joueurs = [Player(input(f"Entrez le nom du joueur {i+1}: ")) for i in range(NbreJoueurs)]
+    
+    return Joueurs
+
+def Tour(Joueurs): # la variqble Joueurs est une liste de joueurs
+    JoueurActuel = 0
+    direction = 1
+    print(f"C'est au tour de {Joueurs[JoueurActuel].name}.") # Chaque joueur a un numéro attribué. La fonction .name permet d'afficher le nom du joueur
+    print(f"Carte en jeu : {CarteJouee}")
+    # Affichage de la main du joueur
+    print("Votre main :")
+    for i, card in enumerate(Joueurs[JoueurActuel].hand): # print le deck du joueur actuel
+        print(f"{i+1}: {card['color']} {card['value']}")
+
+def Verifier(CarteJouee, deck): #retourne une liste de cartes jouables (ou none si aucune carte n'est jouable)
+    return cartes
+
+def Veifier2(): #retourne False si la carte ne peut pas être jouée, True si elle peut être jouée
+    return cartes    
+def main():
+    initialisation()
+    while True:
+        Tour()
+    
+if __name__ == "__main__":
+    main()
